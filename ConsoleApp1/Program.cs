c3a1ffab496d1a735705bcbad1c4c01d5fa6810b
victor palacios <gukuvictor123@gmail.com>
2026-02-16 12:09:50 -0500

    Subiendo ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 25; 
+        decimal height = 1.75m;
+        bool is_student = true;
+        string name = "Ana";
+        char grade = 'A';
+        Console.WriteLine($" age es igual a : {age} \n  height tiene un valor de : {height} \n  el valor de is_student es : {is_student} \n  el nombre es : {name} \n  el caracte es : {grade} \n   ");
+    }
+}
